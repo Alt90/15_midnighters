193be3a24e26e8fb406a03a66c2f46302e64e0f3
Alt13 <Alt90@rambler.ru>
2016-12-27 09:43:42 +0300

    Запилена работа с апи девмана

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,32 @@
+import requests
+
+
+API_URL = 'https://devman.org/api/challenges/solution_attempts/'
+
+
+def get_json_by_page_number(page):
+    api_page = requests.get(API_URL, params={'page': page})
+    if api_page.status_code == 200 :
+        return api_page.json()
+    else:
+        return {}
+
+
+def get_records_by_page_number(page):
+    return get_json_by_page_number(page).get('records', [])
+
+
+def get_count_page():
+    return int(get_json_by_page_number(1).get('number_of_pages', 1))
+
+
 def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+    for page in range(get_count_page()):
+        for records in get_records_by_page_number(page):
+            yield records
 
 def get_midnighters():
     pass
 
 if __name__ == '__main__':
-  pass
+    task_attempts_datas = load_attempts()
